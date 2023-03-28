0206104b37e74bf7d91fb45bcb7fad5b144a517a
SeowooNote <dkr0210@naver.com>
2023-03-28 22:52:00 +0900

    update
    update

---
+++ python/test.py
@@ -0,0 +1,34 @@
+import statistics
+score = [50, 60, 86, 65, 74]
+average = statistics.mean(score)
+print(average)
+
+if score < average:
+        print('불합격')
+else:
+    print('합격')
+
+
+list1 = list()
+print(list1)
+list1.append(100)
+print(list1)
+list2 = [ ]
+print(list2)
+list2.extend([4, 7, 5, 9])
+print(list2)
+list2.sort(reverse=True)
+print(list2)
+list2.remove(max(list2))
+print(list2)
+
+# print(bool(0))
+# print(bool(None))
+# # print(bool({‘a’:1}))
+# # print(bool(“python”))
+# print(bool(2<1))
+
+a = {dic[]'[studnet_number]': 2116243}
+print(a)
+dic = {'name':'pey', 'phone':'010-9999-1234', 'birth': '1118'}
+dic['phone']
